b913187a7fe01647a1d7fdaf35c2bccf2a050e97
Sergey Vinichenko <winechess@gmail.com>
2015-07-15 16:03:39 +0300

    Добавлен класс с настройками приложения и список стадий разработки в которых оно может находиться

---
+++ src/main/java/com/impulsm/fssp/config/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.impulsm.fssp.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by vinichenkosa on 08/07/15.
+ */
+public class ApplicationConfig {
+
+    public static final ProjectStage PROJECT_STAGE;
+    private final static Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
+
+    static {
+        String projectStage = null;
+        try (InputStream is = ApplicationConfig.class.getClass().getResourceAsStream("application.properties")) {
+            Properties p = new Properties();
+            p.load(is);
+            projectStage = p.getProperty("PROJECT_STAGE");
+        } catch (IOException e) {
+            logger.error("Не удалось загрузить свойства из фалйа application.properties");
+        }
+        PROJECT_STAGE = ProjectStage.valueOf(projectStage);
+    }
+
+
+}
---
+++ src/main/java/com/impulsm/fssp/config/ProjectStage.java
@@ -0,0 +1,10 @@
+package com.impulsm.fssp.config;
+
+/**
+ * Created by vinichenkosa on 15/07/15.
+ */
+public enum ProjectStage {
+
+    DEVELOPMENT, PRODUCTION, TEST
+
+}
