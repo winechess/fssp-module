579e80ff2f27d0a20546fc41c8789dc6a9cbabfa
Sergei Vinichenko <winechess@gmail.com>
2015-07-17 03:26:29 +0300

    Добавлен тест для выгрузки всех реестров

---
+++ src/test/java/com/impulsm/fssp/logic/impl/documents/executive/RegistryHandlerImplTest.java
@@ -0,0 +1,16 @@
+package com.impulsm.fssp.logic.impl.documents.executive;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vinichenkosa on 16.07.15.
+ */
+public class RegistryHandlerImplTest {
+
+    @Test
+    public void testHandleAllRegistries() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/impulsm/fssp/utils/impl/FSSPSignatureUtilImplTest.java
@@ -0,0 +1,16 @@
+package com.impulsm.fssp.utils.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vinichenkosa on 16.07.15.
+ */
+public class FSSPSignatureUtilImplTest {
+
+    @Test
+    public void testPkcs7sign() throws Exception {
+
+    }
+}
